ae8eb6b8bb0b0bf251b07c52950f41a19f1b0dad
Petar Panayotov <dakona98@gmail.com>
2023-03-05 20:19:21 +0200

    Added routes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from "./components/Footer";
-import Inputs from "./components/Inputs";
+import Inputs from "./components/power-angle equation/Inputs";
 import MenuAppBar from "./components/Navigation";
+import RotorAngle from './components/rotor-angle/Inputs';
 
 function App() {
   return (
     <div>
-      <MenuAppBar></MenuAppBar>
-      <Inputs></Inputs>
-      <Footer></Footer>
+      <MenuAppBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Inputs />} />
+          <Route path="/rotor-angle" element={<RotorAngle />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   )
 }
