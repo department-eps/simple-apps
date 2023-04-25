d01411d44d8434f64dd6299c3b2d284434dd1a53
Petar Panayotov <dakona98@gmail.com>
2023-04-25 22:36:20 +0300

    Styles refactor. Fixed labels and adornments.

--- src/components/RotorAngle/Inputs/RotorAngleInputs.js
+++ src/components/RotorAngle/Inputs/RotorAngleInputs.js
@@ -1,9 +1,10 @@
-import { TextField, Grid, Typography, Tooltip, Container, Box } from "@mui/material";
+import { TextField, Grid, Typography, Tooltip, Container, Box, InputAdornment } from "@mui/material";
+import styles from "./RotorAngleInputs.module.css"
 import Plot from "../Plot/RotorAnglePlot";
 import { useForm } from "../../../hooks/useForm";
 
 export default function RotorAngle() {
-    const {formValues, onChange} = useForm({
+    const { formValues, onChange } = useForm({
         Ra: "0.004",
         Xd: "0.3",
         U1: "1",
@@ -14,115 +15,136 @@ export default function RotorAngle() {
     });
 
     return (
-        <Container sx={{ backgroundColor: 'white', paddingTop: '80px' }}>
+        <Container className={styles['container']}>
             <Grid
                 container
                 spacing={2}
-                sx={{ justifyContent: "center", alignItems: "center" }}
+                className={styles['wrapper']}
             >
-                <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                <Grid item xs={12} className={styles['item']}>
                     <Typography variant="h5">Generator:</Typography>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={2}>
-                    <Box sx={{ textAlign: "center" }}>
-                        <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the Ra field</Typography>}>
+                    <Box className={styles['item']}>
+                        <Tooltip arrow title={<Typography variant="subtitle2">This is the Ra field</Typography>}>
                             <TextField
+                                className={styles['input-field']}
                                 name="Ra"
-                                label="Ra [pu]"
+                                label={<span className={styles['input-label']}>Ra</span>}
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">pu</InputAdornment>,
+                                }}
                                 value={formValues.Ra}
                                 onChange={onChange}
-                                margin="normal"
                                 type="number"
                             />
                         </Tooltip>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={2} >
-                    <Box sx={{ textAlign: "center" }}>
-                        <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the Xd field</Typography>}>
+                    <Box className={styles['item']}>
+                        <Tooltip arrow title={<Typography variant="subtitle2">This is the Xd field</Typography>}>
                             <TextField
+                                className={styles['input-field']}
                                 name="Xd"
-                                label="Xd [pu]"
+                                label={<span className={styles['input-label']}>Xd</span>}
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">pu</InputAdornment>,
+                                }}
                                 value={formValues.Xd}
                                 onChange={onChange}
-                                margin="normal"
                             />
                         </Tooltip>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={2}>
-                    <Box sx={{ textAlign: "center" }}>
-                        <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the U1 field</Typography>}>
+                    <Box className={styles['item']}>
+                        <Tooltip arrow title={<Typography variant="subtitle2">This is the U1 field</Typography>}>
                             <TextField
+                                className={styles['input-field']}
                                 name="U1"
-                                label="U1 [pu]"
+                                label={<span className={styles['input-label']}>U1</span>}
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">pu</InputAdornment>,
+                                }}
                                 value={formValues.U1}
                                 onChange={onChange}
-                                margin="normal"
                                 type="number"
                             />
                         </Tooltip>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={2}>
-                    <Box sx={{ textAlign: "center" }}>
-                        <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the P1 field</Typography>}>
+                    <Box className={styles['item']}>
+                        <Tooltip arrow title={<Typography variant="subtitle2">This is the P1 field</Typography>}>
                             <TextField
+                                className={styles['input-field']}
                                 name="P1"
-                                label="P1 [pu]"
+                                label={<span className={styles['input-label']}>P1</span>}
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">pu</InputAdornment>,
+                                }}
                                 value={formValues.P1}
                                 onChange={onChange}
-                                margin="normal"
                                 type="number"
                             />
                         </Tooltip>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={2}>
-                    <Box sx={{ textAlign: "center" }}>
-                        <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the H field</Typography>}>
+                    <Box className={styles['item']}>
+                        <Tooltip arrow title={<Typography variant="subtitle2">This is the H field</Typography>}>
                             <TextField
+                                className={styles['input-field']}
                                 name="H"
-                                label="H [MW-s/MVA]"
+                                label={<span className={styles['input-label']}>H</span>}
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end"><Typography variant="body2">MW-s/MVA</Typography></InputAdornment>,
+                                }}
                                 value={formValues.H}
                                 onChange={onChange}
-                                margin="normal"
                                 type="number"
                             />
                         </Tooltip>
                     </Box>
                 </Grid>
 
-                <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                <Grid item xs={12} className={styles['item']}>
                     <Typography variant="h5">System:</Typography>
                 </Grid>
 
-                <Grid item xs={12} sm={6} md={6} lg={3} sx={{ textAlign: 'center' }}>
-                    <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the U2 field</Typography>}>
+                <Grid item xs={12} sm={6} md={6} lg={2} className={styles['item']}>
+                    <Tooltip arrow title={<Typography variant="subtitle2">This is the U2 field</Typography>}>
                         <TextField
+                            className={styles['input-field']}
                             name="U2"
-                            label="U2 [pu]"
+                            label={<span className={styles['input-label']}>U2</span>}
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">pu</InputAdornment>,
+                            }}
                             value={formValues.U2}
                             onChange={onChange}
-                            margin="normal"
                             type="number"
                         />
                     </Tooltip>
                 </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={3} sx={{ textAlign: 'center' }}>
-                    <Tooltip arrow title={<Typography sx={{ fontSize: 14 }}>This is the θ field</Typography>}>
+                <Grid item xs={12} sm={6} md={6} lg={2} className={styles['item']}>
+                    <Tooltip arrow title={<Typography variant="subtitle2">This is the θ field</Typography>}>
                         <TextField
+                            className={styles['input-field']}
                             name="thetaU2"
-                            label="θ [deg]"
+                            label={<span className={styles['input-label']}>θ</span>}
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">deg</InputAdornment>,
+                            }}
                             value={formValues.thetaU2}
                             onChange={onChange}
-                            margin="normal"
                             type="number"
                         />
                     </Tooltip>
                 </Grid>
-                <Grid item xs={12} lg={8} md={8} sx={{ textAlign: "center" }}>
-                    <Box sx={{ height: "650px" }}>
+                <Grid item xs={12} lg={9} md={9} className={styles['item']}>
+                    <Box className={styles['plot']}>
                         <Plot values={formValues}></Plot>
                     </Box>
                 </Grid>
