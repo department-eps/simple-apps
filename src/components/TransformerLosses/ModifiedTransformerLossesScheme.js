615ddc831df9b0da2514fc359a0691febf7ddfc8
Petar Panayotov <dakona98@gmail.com>
2023-05-03 20:15:50 +0300

    Updated svg view.

--- src/components/TransformerLosses/ModifiedTransformerLossesScheme.js
+++ src/components/TransformerLosses/ModifiedTransformerLossesScheme.js
@@ -3,11 +3,11 @@ import { ReactComponent as TransformerLossesScheme } from './transformerLossesSc
 
 export default function ModifiedTransformerLossesScheme() {
     return (
-        <svg width="450" height="255" style={{ width: "100%", height: "auto", maxWidth: "100%" }} viewBox="0 0 900 500" preserveAspectRatio="xMidYMid meet">
+        <svg width="450" height="100" style={{ width: "100%", height: "auto", maxWidth: "100%" }} viewBox="0 50 900 150" preserveAspectRatio="xMidYMid meet">
             <TransformerLossesScheme />
             <foreignObject x="353" y="28" width="250px" height="40">
                 <div xmlns="http://www.w3.org/1999/xhtml">
-                    <Typography sx={{ fontSize: '20pt' }}>loading = 2.1%</Typography>
+                    <Typography sx={{ fontSize: '20pt' }}>loading = 2.1 %</Typography>
                 </div>
             </foreignObject>
             <foreignObject x="180" y="25" width="250px" height="40">
