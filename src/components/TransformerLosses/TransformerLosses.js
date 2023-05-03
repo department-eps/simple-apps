1e8e67f6ebfdeda83ffa10ade256e9fe6b0937d7
Petar Panayotov <dakona98@gmail.com>
2023-05-03 20:16:16 +0300

    Update TransformerLosses.js

--- src/components/TransformerLosses/TransformerLosses.js
+++ src/components/TransformerLosses/TransformerLosses.js
@@ -1,12 +1,22 @@
 import { Container, Grid } from '@mui/material';
 import ModifiedTransformerLossesScheme from './ModifiedTransformerLossesScheme';
+import Sliders from './Sliders';
+import RadioButtons from './RadioButtons';
 
 export default function TransformerLosses() {
     return (
         <Container maxWidth={'lg'} sx={{ paddingTop: '90px' }}>
             <Grid container sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>  
-                        <ModifiedTransformerLossesScheme />
+                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>
+                    <ModifiedTransformerLossesScheme />
+                </Grid>
+                <Grid item xs={6} md={3} lg={3} sx={{ textAlign: 'center' }}>
+
+                    <RadioButtons />
+                </Grid>
+                <Grid item xs={6} md={3} lg={3} sx={{ textAlign: 'center' }}>
+
+                    <Sliders></Sliders>
                 </Grid>
             </Grid>
         </Container>
