64dc90d870c5e00795d98ebcef8547889b0bf012
Petar Panayotov <dakona98@gmail.com>
2023-05-08 01:36:22 +0300

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,38 @@
+export const U1options = [{
+    value: 18,
+    label: '18',
+},
+{
+    value: 20,
+    label: '20',
+},
+{
+    value: 22,
+    label: '22',
+}];
+
+export const S1options = [{
+    value: 0,
+    label: '0'
+},
+{
+    value: 500,
+    label: '500',
+},
+{
+    value: 1000,
+    label: '1000'
+}];
+
+export const cosphioptions = [{
+    value: 0,
+    label: '0'
+},
+{
+    value: 0.5,
+    label: '0.5',
+},
+{
+    value: 1,
+    label: '1'
+}];
