a0307728fc5f56cec55d8d68df2a82cfb2c32192
Petar Panayotov <dakona98@gmail.com>
2023-05-17 20:33:59 +0300

    Code optimization. Added styles

--- src/components/PowerLineLosses/PowerLineLosses.js
+++ src/components/PowerLineLosses/PowerLineLosses.js
@@ -1,52 +1,74 @@
-import { Container, Grid, Typography } from "@mui/material";
-import { ActivePower, ActiveResistance, LongitudinalReactiveResistance, LongitudinalActiveResistance, NominalVoltage, PowerLineLossEquation, ReactivePower, ReactiveResistance, ActiveResistanceForKm, ReactiveResistanceForKm, LengthLine } from "./Equations";
-import RadioButtons from "./RadioButtons";
+import { Box, Container, Grid, StyledEngineProvider, Typography } from "@mui/material";
+import { ActivePower, ActiveResistance, LongitudinalReactiveResistance, LongitudinalActiveResistance, NominalVoltage, PowerLineLossEquation, ReactivePower, ReactiveResistance, ActiveResistanceForKm, ReactiveResistanceForKm, LengthLine } from "./Equations/Equations";
+import RadioButtons from "./Inputs/RadioButtons";
 import { useState } from "react";
-import Sliders from "./Sliders";
+import Sliders from "./Inputs/Sliders";
+import Chart from "./Charts/Chart";
+import styles from "./PowerLineLosses.module.css";
+import ModifiedPowerLineLossesScheme from "./Scheme/ModifiedPowerLineLossesScheme";
 
 export default function PowerLineLosses() {
     const [radioValue, setRadioValue] = useState('all');
+    const [voltageLosses, setVoltageLosses] = useState([]);
+    const [currentVoltageLosses, setCurrentVoltageLosses] = useState({})
 
     return (
-        <Container maxWidth={'lg'} sx={{ paddingTop: '90px' }}>
-            <Grid container spacing={2} sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>
-                    <Typography variant="h3" fontFamily={"sans-serif"}>Изчисляване на загуби на напрежение в надлъжен елемент (електропровод)</Typography>
+        <StyledEngineProvider injectFirst>
+            <Container maxWidth={'lg'} className={styles['container']}>
+                <Grid container spacing={2} className={styles['wrapper']}>
+                    <Grid item xs={12} md={12} lg={12} className={styles['item-center']}>
+                        <Typography variant="h3">Изчисляване на загуби на напрежение в надлъжен елемент (електропровод)</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={12} className={styles['item-center']}>
+                        <PowerLineLossEquation />
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={6} className={styles['grid']}>
+                        <Box>
+                            <ActivePower />
+                            <ReactivePower />
+                            <ActiveResistance />
+                            <ReactiveResistance />
+                            <NominalVoltage />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={6} className={styles['grid']}>
+                        <Box>
+                            <LongitudinalActiveResistance />
+                            <LongitudinalReactiveResistance />
+                            <ActiveResistanceForKm />
+                            <ReactiveResistanceForKm />
+                            <LengthLine />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={12}>
+                        <Box paddingTop={5}>
+                            <div>Положителни <span className={styles['katex-label']}>P, Q</span> означават консумация на мощност.</div>
+                            <div>Отрицателни <span className={styles['katex-label']}>P, Q</span> означават генерация на мощност (например PV централа).</div>
+                            <div style={{paddingTop: '50px'}}>Изследвайте как загубите на напрежение се влияят от:</div>
+                            <ul style={{ fontSize: '20px' }}>
+                                <li>Активна мощност на товара</li>
+                                <li>Реактивна мощност на товара</li>
+                                <li>Дължината (т.е импеданса) на товара</li>
+                            </ul>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={12} className={styles['item-ceneter']}>
+                        <ModifiedPowerLineLossesScheme currentVoltageLosses={currentVoltageLosses} />
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={3} className={styles['item-center']}>
+                        <RadioButtons radioValue={radioValue} setRadioValue={setRadioValue} />
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={3} className={styles['item-center']}>
+                        <Sliders radioValue={radioValue} setVoltageLosses={setVoltageLosses} setCurrentVoltageLosses={setCurrentVoltageLosses} />
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>
+                        <Typography className={styles['chart-label']}>
+                            <span className={styles['katex-label']}>ΔU</span><span> [V]</span>
+                        </Typography>
+                        <Chart voltageLosses={voltageLosses} radioValue={radioValue} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>
-                    <PowerLineLossEquation />
-                </Grid>
-                <Grid item xs={12} md={6} lg={6} sx={{ textAlign: 'left' }}>
-                    <ActivePower />
-                    <ReactivePower />
-                    <ActiveResistance />
-                    <ReactiveResistance />
-                    <NominalVoltage />
-                </Grid>
-                <Grid item xs={12} md={6} lg={6} sx={{ textAlign: 'left' }}>
-                    <LongitudinalActiveResistance />
-                    <LongitudinalReactiveResistance />
-                    <ActiveResistanceForKm />
-                    <ReactiveResistanceForKm />
-                    <LengthLine />
-                </Grid>
-                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'left', fontFamily: 'sans-serif' }}>
-                    <Typography sx={{ fontSize: '20px' }}>Положителни <span className="katex mathnormal" style={{ fontStyle: 'italic' }}>P, Q</span> означават консумация на мощност.</Typography>
-                    <Typography sx={{ fontSize: '20px' }}>Отрицателни <span className="katex mathnormal" style={{ fontStyle: 'italic' }}>P, Q</span> означават генерация на мощност (например PV централа).</Typography>
-                    <Typography sx={{ fontSize: '20px', paddingTop: '20px' }}>Изследвайте как загубите на напрежение се влияят от:</Typography>
-                    <ul style={{fontSize: '20px'}}>
-                        <li>Активна мощност на товара</li>
-                        <li>Реактивна мощност на товара</li>
-                        <li>Дължината (т.е импеданса) на товара</li>
-                    </ul>
-                </Grid>
-                <Grid item xs={12} md={6} lg={3} sx={{ textAlign: 'center', fontFamily: 'sans-serif' }}>
-                    <RadioButtons radioValue={radioValue} setRadioValue={setRadioValue} />
-                </Grid>
-                <Grid item xs={12} md={6} lg={3} sx={{ textAlign: 'center', fontFamily: 'sans-serif' }}>
-                    <Sliders radioValue={radioValue} />
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </StyledEngineProvider>
     );
 };
