dff34d612c7083521d754bf84966f8cd7cc1c93f
Petar Panayotov <dakona98@gmail.com>
2023-05-21 20:37:34 +0300

    Update ModifiedKirchhoffScheme.js

--- src/components/Kirchhoff/Scheme/ModifiedKirchhoffScheme.js
+++ src/components/Kirchhoff/Scheme/ModifiedKirchhoffScheme.js
@@ -24,58 +24,65 @@ export default function ModifiedKirchhoffScheme() {
     }, [formValues])
 
     return (
-        <svg width="450" height="255" style={{ width: "100%", height: "auto", maxWidth: "100%" }} viewBox="0 0 900 500" preserveAspectRatio="xMidYMid meet">
+        <svg width="450" height="155" style={{ width: "100%", height: "auto", maxWidth: "100%" }} viewBox="0 150 900 50" preserveAspectRatio="xMidYMid meet">
             <KirchhoffScheme />
-            {nodes.node1 && nodes.node2 && nodes.node3 &&
+            {typeof nodes.node1 !== 'undefined' && !isNaN(nodes.node1) &&
+                typeof nodes.node2 !== 'undefined' && !isNaN(nodes.node2) &&
+                typeof nodes.node3 !== 'undefined' && !isNaN(nodes.node3) &&
                 <>
-                    <foreignObject x="242" y="40" width="70px" height="40">
+                    <foreignObject x="215" y="35" width="130px" height="40">
                         <div xmlns="http://www.w3.org/1999/xhtml">
-                            <Typography variant='h5'>{nodes.node1} kW</Typography>
+                            <Typography sx={{ fontSize: '20pt', fontFamily: 'Times New Roman' }}>{nodes.node1} kW</Typography>
                         </div>
                     </foreignObject>
-                    <foreignObject x="462" y="40" width="70px" height="40">
+                    <foreignObject x="435" y="35" width="130px" height="40">
                         <div xmlns="http://www.w3.org/1999/xhtml">
-                            <Typography variant='h5'>{nodes.node2} kW</Typography>
+                            <Typography sx={{ fontSize: '20pt', fontFamily: 'Times New Roman' }}>{nodes.node2} kW</Typography>
                         </div>
                     </foreignObject>
-                    <foreignObject x="667" y="40" width="70px" height="40">
+                    <foreignObject x="642" y="35" width="130px" height="40">
                         <div xmlns="http://www.w3.org/1999/xhtml">
-                            <Typography variant='h5'>{nodes.node3} kW</Typography>
+                            <Typography sx={{ fontSize: '20pt', fontFamily: 'Times New Roman' }}>{nodes.node3} kW</Typography>
                         </div>
                     </foreignObject>
                 </>
             }
-            <foreignObject x="377" y="230" width="80px" height="40">
+            <foreignObject x="371" y="230" width="100px" height="40">
                 <div xmlns="http://www.w3.org/1999/xhtml" style={{ display: 'flex' }}>
                     <input
-                        style={{ width: '30px', fontSize: '23px', textAlign: 'center' }}
+                        style={{ width: '40px', fontSize: '20pt', textAlign: 'center', fontFamily: 'Times New Roman' }}
                         name='B1'
                         value={formValues.B1}
                         onChange={onChange}
+                        inputMode="decimal"
                     ></input>
-                    <Typography sx={{ fontSize: '23px', paddingLeft: '5px' }}>kW</Typography>
+                    <Typography sx={{ fontSize: '19pt', fontFamily: 'Times New Roman', paddingLeft: '10px' }}>kW</Typography>
                 </div>
             </foreignObject>
-            <foreignObject x="583" y="230" width="80px" height="40">
+            <foreignObject x="578" y="230" width="100px" height="40">
                 <div xmlns="http://www.w3.org/1999/xhtml" style={{ display: 'flex' }}>
                     <input
-                        style={{ width: '30px', fontSize: '23px', textAlign: 'center' }}
+                        style={{ width: '40px', fontSize: '20pt', textAlign: 'center', fontFamily: 'Times New Roman' }}
                         name='B2'
                         value={formValues.B2}
                         onChange={onChange}
+                        inputMode="decimal"
+                        lang='en'
                     ></input>
-                    <Typography sx={{ fontSize: '23px', paddingLeft: '5px' }}>kW</Typography>
+                    <Typography sx={{ fontSize: '20pt', fontFamily: 'Times New Roman', paddingLeft: '10px' }}>kW</Typography>
                 </div>
             </foreignObject>
-            <foreignObject x="789" y="230" width="90px" height="40">
+            <foreignObject x="783" y="230" width="100px" height="40">
                 <div xmlns="http://www.w3.org/1999/xhtml" style={{ display: 'flex' }}>
                     <input
-                        style={{ width: '30px', fontSize: '23px', textAlign: 'center' }}
+                        style={{ width: `40px`, fontSize: '20pt', textAlign: 'center', fontFamily: 'Times New Roman' }}
                         name='B3'
                         value={formValues.B3}
-                        onChange={onChange}>
+                        onChange={onChange}
+                        inputMode="decimal"
+                    >
                     </input>
-                    <Typography sx={{ fontSize: '23px', paddingLeft: '5px' }}>kW</Typography>
+                    <Typography sx={{ fontSize: '20pt', fontFamily: 'Times New Roman', paddingLeft: '10px' }}>kW</Typography>
                 </div>
             </foreignObject>
         </svg>
