b50fdc3d633aba15ae13ca891583fedc51754bcb
Petar Panayotov <dakona98@gmail.com>
2023-05-21 20:38:00 +0300

    Added Kirchhoff validation.

--- src/components/Kirchhoff/Kirchhoff.js
+++ src/components/Kirchhoff/Kirchhoff.js
@@ -5,11 +5,11 @@ export default function Kirchhoff() {
     return (
         <Container maxWidth={'lg'} sx={{ paddingTop: '90px' }}>
             <Grid container sx={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>
-                    <Typography variant='h3'>Изчисляване на просто потокоразпределение в мрежата в мрежа ниско напрежение (без отчитане на загубите в линиите)</Typography>
+                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center'}}>
+                    <Typography variant='h3'>Изчисляване на просто потоко&shy;разпределение в мрежа НН. (без отчитане на загубите в линиите)</Typography>
                 </Grid>
-                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'left', paddingTop: '15px' }}>
-                    <Typography variant='h5'>&emsp;Въведете мощностите на товарите във всеки възел. При това ще видите как се променят мощностите във всеки клон на мрежата съгласно |-ви закон на Кирхоф.</Typography>
+                <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'left', paddingTop: '35px' }}>
+                    <Typography variant='h5'>&emsp;Въведете мощностите на товарите във всеки възел. При това ще видите как се променят мощностите във всеки клон на мрежата съгласно I-ви закон на Кирхоф.</Typography>
                     <Typography variant='h5' sx={{ paddingTop: '15px' }}>&emsp;Примерът е за потокоразпределение на активните мощности, но същият принцип се прилага и за разпределение на реактивните мощности.</Typography>
                 </Grid>
                 <Grid item xs={12} md={12} lg={12} sx={{ textAlign: 'center' }}>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -5,6 +5,14 @@ export function useForm(initialValues) {
     const [formValues, setFormValues] = useState(initialValues);
 
     const onChange = (e) => {
+        if (e.target.name === 'B1' || e.target.name === 'B2' || e.target.name === 'B3') {
+            const numbers = e.target.value.match(/\d/g)?.length;
+            const dotCount = (e.target.value.match(/\./g) || []).length;
+            const hyphenCount = (e.target.value.match(/-/g) || []).length;
+            if (numbers > 3 || dotCount > 1 || (hyphenCount > 1 || (hyphenCount === 1 && e.target.value[0] !== '-'))) {
+              return;
+            }
+          }
         if (e.target.value === "" || regex.test(e.target.value)) {
             setFormValues({
                 ...formValues,
