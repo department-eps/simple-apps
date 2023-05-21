227c7f8dbff6f3831a6f4774ecd7eac7b7b019e9
Petar Panayotov <dakona98@gmail.com>
2023-05-21 16:55:50 +0300

    Increased font size. Centered.

--- src/components/PowerLineLosses/Scheme/ModifiedPowerLineLossesScheme.js
+++ src/components/PowerLineLosses/Scheme/ModifiedPowerLineLossesScheme.js
@@ -9,28 +9,28 @@ export default function ModifiedPowerLineLossesScheme() {
     return (
         <svg width="450" height="100" style={{ width: "100%", height: "auto", maxWidth: "100%", fontFamily: 'Times New Roman' }} viewBox="0 50 900 150" preserveAspectRatio="xMidYMid meet">
             <PowerLineLossesScheme />
-            <foreignObject x="385" y="58" width="250px" height="40">
-                <div xmlns="http://www.w3.org/1999/xhtml">
+            <foreignObject x="306" y="65" width="330px" height="40">
+                <div xmlns="http://www.w3.org/1999/xhtml" style={{fontSize: '20pt', textAlign: 'center'}}>
                     <InlineMath math={'\\Delta U'} /> = {((currentVoltageLosses.lineLosses?.deltaU? (currentVoltageLosses.lineLosses?.deltaU).toFixed(2): 0))} V ({currentVoltageLosses.lineLosses?.deltaUInPercent}%)
                 </div>
             </foreignObject>
-            <foreignObject x="426" y="168" width="250px" height="40">
-                <div xmlns="http://www.w3.org/1999/xhtml">
+            <foreignObject x="409" y="158" width="250px" height="40">
+                <div xmlns="http://www.w3.org/1999/xhtml" style={{fontSize: '20pt'}}>
                     <InlineMath math={'R'} /> = {(currentVoltageLosses.lineLosses?.R? (currentVoltageLosses.lineLosses.R).toFixed(2): 0)} Ω
                 </div>
             </foreignObject>
-            <foreignObject x="423" y="198" width="250px" height="40">
+            <foreignObject x="403" y="188" width="250px" height="40" style={{fontSize: '20pt'}}>
                 <div xmlns="http://www.w3.org/1999/xhtml">
                     <InlineMath math={'X'} /> = {(currentVoltageLosses.lineLosses?.X? (currentVoltageLosses.lineLosses.X).toFixed(2): 0)} Ω
                 </div>
             </foreignObject>
-            <foreignObject x="280" y="25" width="250px" height="50">
-                <div xmlns="http://www.w3.org/1999/xhtml">
-                    400 V
+            <foreignObject x="260" y="20" width="250px" height="50">
+                <div xmlns="http://www.w3.org/1999/xhtml" style={{fontSize: '20pt'}}>
+                    400.00 V
                 </div>
             </foreignObject>
-            <foreignObject x="610" y="25" width="250px" height="50">
-                <div xmlns="http://www.w3.org/1999/xhtml">
+            <foreignObject x="520" y="20" width="250px" height="50">
+                <div xmlns="http://www.w3.org/1999/xhtml" style={{fontSize: '20pt', textAlign: 'center'}}>
                     {currentVoltageLosses.lineLosses?.U2} V
                 </div>
             </foreignObject>
