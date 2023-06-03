488bff63d04dc5497e940efeec00ea24f8a2a2e9
Petar Panayotov <dakona98@gmail.com>
2023-06-03 17:27:07 +0300

    Update PowerAngleInputs.js

--- src/components/PowerAngle/Inputs/PowerAngleInputs.js
+++ src/components/PowerAngle/Inputs/PowerAngleInputs.js
@@ -1,8 +1,8 @@
-import { TextField, Grid, Container, Tooltip, Typography, Box, InputAdornment } from "@mui/material";
+import { TextField, Grid, Container, Tooltip, Typography, Box, InputAdornment, StyledEngineProvider } from "@mui/material";
 import styles from "./PowerAngleInputs.module.css"
 import Plot from "../Plot/PowerAnglePlot";
 import { useForm } from "../../../hooks/useForm";
-
+import { InlineMath } from "react-katex";
 export default function PowerAngle() {
 
     const { formValues, onChange } = useForm({
@@ -13,90 +13,96 @@ export default function PowerAngle() {
     });
 
     return (
-        <Container className={styles['container']}>
-            <Grid
-                container
-                spacing={2}
-                className={styles['wrapper']}
-            >
-                <Grid item xs={12} sm={8} md={6} lg={2}>
-                    <Tooltip arrow title={<Typography variant="subtitle2">This is Us. Write tooltip here</Typography>}>
+        <StyledEngineProvider injectFirst>
+            <Container className={styles['container']}>
+                <Grid
+                    container
+                    spacing={2}
+                    className={styles['wrapper']}
+                >
+                    <Grid item xs={12} sm={8} md={6} lg={2}>
                         <Box className={styles['item']}>
                             <TextField
                                 className={styles['input-field']}
+                                variant="standard"
                                 name="Us"
-                                label={<span className={styles['input-label']}>Us</span>}
                                 InputProps={{
                                     endAdornment: <InputAdornment position="end">p.u</InputAdornment>,
+                                    sx: { fontSize: '20px' }
                                 }}
+                                label={<span style={{ fontSize: '20px' }}><InlineMath math={'U_s'}></InlineMath></span>}
                                 value={formValues.Us}
                                 onChange={onChange}
                                 type="text"
-                                autoComplete='off'
+                                autoComplete="off"
+                                sx={{ "&.MuiFormLabel-root": { "&.MuiInputLabel-root": { color: 'red' } } }}
                             />
                         </Box>
-                    </Tooltip>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6} lg={2}>
+                        <Box className={styles['item']}>
+                            <Tooltip arrow title={<Typography variant="subtitle2">This is Ur. Write tooltip here</Typography>}>
+                                <TextField
+                                    className={styles['input-field']}
+                                    variant="standard"
+                                    name="Ur"
+                                    label={<span><InlineMath math={'U_r'}></InlineMath></span>}
+                                    InputProps={{
+                                        endAdornment: <InputAdornment position="end">p.u</InputAdornment>,
+                                    }}
+                                    value={formValues.Ur}
+                                    onChange={onChange}
+                                    type="text"
+                                    autoComplete='off'
+                                />
+                            </Tooltip>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6} lg={2}>
+                        <Box className={styles['item']}>
+                            <Tooltip arrow title={<Typography variant="subtitle2">This is X. Write tooltip here</Typography>}>
+                                <TextField
+                                    className={styles['input-field']}
+                                    variant="standard"
+                                    name="X"
+                                    label={<span><InlineMath math={'X'}></InlineMath></span>}
+                                    InputProps={{
+                                        endAdornment: <InputAdornment position="end">p.u</InputAdornment>,
+                                    }}
+                                    value={formValues.X}
+                                    onChange={onChange}
+                                    type="text"
+                                    autoComplete='off'
+                                />
+                            </Tooltip>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={8} md={6} lg={2}>
+                        <Box className={styles['item']}>
+                            <Tooltip arrow title={<Typography variant="subtitle2">This is R. Write tooltip here</Typography>}>
+                                <TextField
+                                    className={styles['input-field']}
+                                    variant="standard"
+                                    name="R"
+                                    label={<span><InlineMath math={'R'}></InlineMath></span>}
+                                    InputProps={{
+                                        endAdornment: <InputAdornment position="end">p.u</InputAdornment>,
+                                    }}
+                                    value={formValues.R}
+                                    onChange={onChange}
+                                    type="text"
+                                    autoComplete='off'
+                                />
+                            </Tooltip>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} lg={8} md={8} className={styles['item']}>
+                        <Box className={styles['plot']}>
+                            <Plot values={formValues} />
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={8} md={6} lg={2}>
-                    <Box className={styles['item']}>
-                        <Tooltip arrow title={<Typography variant="subtitle2">This is Ur. Write tooltip here</Typography>}>
-                            <TextField
-                                className={styles['input-field']}
-                                name="Ur"
-                                label={<span className={styles['input-label']}>Ur</span>}
-                                InputProps={{
-                                    endAdornment: <InputAdornment position="end">p.u</InputAdornment>,
-                                }}
-                                value={formValues.Ur}
-                                onChange={onChange}
-                                type="text"
-                                autoComplete='off'
-                            />
-                        </Tooltip>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={6} lg={2}>
-                    <Box className={styles['item']}>
-                        <Tooltip arrow title={<Typography variant="subtitle2">This is X. Write tooltip here</Typography>}>
-                            <TextField
-                                className={styles['input-field']}
-                                name="X"
-                                label={<span className={styles['input-label']}>X</span>}
-                                InputProps={{
-                                    endAdornment: <InputAdornment position="end">p.u</InputAdornment>,
-                                }}
-                                value={formValues.X}
-                                onChange={onChange}
-                                type="text"
-                                autoComplete='off'
-                            />
-                        </Tooltip>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={6} lg={2}>
-                    <Box className={styles['item']}>
-                        <Tooltip arrow title={<Typography variant="subtitle2">This is R. Write tooltip here</Typography>}>
-                            <TextField
-                                className={styles['input-field']}
-                                name="R"
-                                label={<span className={styles['input-label']}>R</span>}
-                                InputProps={{
-                                    endAdornment: <InputAdornment position="end"><span>p.u</span></InputAdornment>,
-                                }}
-                                value={formValues.R}
-                                onChange={onChange}
-                                type="text"
-                                autoComplete='off'
-                            />
-                        </Tooltip>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} lg={8} md={8} className={styles['item']}>
-                    <Box className={styles['plot']}>
-                        <Plot values={formValues} />
-                    </Box>
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </StyledEngineProvider>
     );
 };
