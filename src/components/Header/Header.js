06014567050521730a570c2eba7873e71c218069
Petar Panayotov <dakona98@gmail.com>
2023-06-03 17:26:58 +0300

    Added navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,83 @@
-import { AppBar, Box, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Typography, Drawer, List, ListItem } from "@mui/material";
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { FirstPage } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 export default function Header() {
-    const [menu, setMenu] = useState(null)
+    const [isDrawerOpen, setDrawerOpen] = useState(false);
+    const location = useLocation();
+    
+    useEffect(() => {
+        setDrawerOpen(false);
+    }, [location]);
 
-    const openMenu = (event) => {
-        setMenu(event.currentTarget);
+    const toggleDrawer = () => {
+        setDrawerOpen(!isDrawerOpen);
     };
 
-    const closeMenu = () => {
-        setMenu(null);
+    const closeDrawer = () => {
+        setDrawerOpen(false)
     };
 
     return (
         <Box>
             <AppBar sx={{ bgcolor: "#dedede" }} elevation={3}>
                 <Toolbar>
-                    <Box sx={{marginLeft: '-20px'}}>
+                    <Box sx={{ marginLeft: '-20px' }}>
                         <IconButton
                             size="large"
-                            onClick={openMenu}
+                            onClick={toggleDrawer}
                         >
                             <MenuOutlinedIcon
                                 sx={{ fontSize: '30px', color: 'black' }}
                             />
                         </IconButton>
-                        <Menu
-                            open={Boolean(menu)}
-                            anchorEl={menu}
-                            onClose={closeMenu}
-                        >
-                            <MenuItem><Typography variant="subtitle1">Power-Angle eqution</Typography></MenuItem>
-                            <MenuItem><Typography variant="subtitle1">Rotor-Angle eqution</Typography></MenuItem>
-                            
-                        </Menu>
                     </Box>
                     <Typography variant="h5" sx={{ color: 'black' }}>Tasks</Typography>
                 </Toolbar>
             </AppBar>
+            <Drawer
+                open={isDrawerOpen}
+                onClose={toggleDrawer}>
+                <div style={{ borderBottom: '1px solid #186092', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <FirstPage sx={{ color: '#186092', fontSize: '60px', height: '100%' }} onClick={closeDrawer} />
+                    <img src="https://ee.tu-varna.bg/wp-content/uploads/2020/03/Logo_Colour_TUV.png" style={{ width: '300px', marginLeft: 'auto', marginRight: 'auto' }} />
+                </div>
+                <List>
+                    <div style={{ borderBottom: '1px solid #186092'}}>
+                        <Typography variant="h5">Устойчивост на ЕЕС</Typography>
+                        <Link to="/" style={{textDecoration: 'none', color: 'black'}}>
+                            <ListItem sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 0, 0.2)' } }}>
+                                <Typography sx={{ fontSize: '18px' }}>Устойчивост по роторен ъгъл</Typography>
+                            </ListItem>
+                        </Link>
+                        <Link to={'/rotor-angle'} style={{textDecoration: 'none', color: 'black'}}>
+                            <ListItem sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 0, 0.2)' } }}>
+                                <Typography sx={{ fontSize: '18px' }}>Ъглова харектеристика</Typography>
+                            </ListItem>
+                        </Link>
+                    </div>
+                    <div style={{ borderBottom: '1px solid #186092'}}>
+                        <Typography variant="h5">Ел. мрежи и системи</Typography>
+                        <Link to={"/power-line-losses"} style={{textDecoration: 'none', color: 'black'}}>
+                            <ListItem sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 0, 0.2)' } }}>
+                                <Typography sx={{ fontSize: '18px' }}>Загуби на напрежение в електропровод</Typography>
+                            </ListItem>
+                        </Link>
+                        <Link to={"/transformer-losses"} style={{textDecoration: 'none', color: 'black'}}>
+                            <ListItem sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 0, 0.2)' } }}>
+                                <Typography sx={{ fontSize: '18px' }}>Загуби на мощност в трансформатор</Typography>
+                            </ListItem>
+                        </Link>
+                        <Link to={"/kirchhoff"} style={{textDecoration: 'none', color: 'black'}}>
+                            <ListItem sx={{ '&:hover': { backgroundColor: 'rgba(0, 0, 0, 0.2)' } }}>
+                                <Typography sx={{ fontSize: '18px' }}>Изчисляване на просто потокоразпределение</Typography>
+                            </ListItem>
+                        </Link>
+                    </div>
+                </List>
+            </Drawer>
         </Box>
-    )
-}
+    );
+};
